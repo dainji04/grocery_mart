5c013d3264add792ebb4ccaec26ed4f658da87a9
dainji4get <ndai31072004@gmail.com>
2024-01-05 13:29:38 +0700

    complete  home app grocery

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,20 @@
   <div id="app">
     <DefaultHeader />
     <BodyContent />
+    <Footer />
   </div>
 </template>
 
 <script>
 import DefaultHeader from '@/components/layout/components/header/header.vue';
 import BodyContent from '@/components/layout/components/body/body.vue';
+import Footer from '@/components/layout/components/footer/footer.vue';
 export default {
   name: 'app',
   components: {
     DefaultHeader,
     BodyContent,
+    Footer,
   },
 };
 </script>
---
+++ src/components/layout/components/footer/footer.vue
@@ -0,0 +1,87 @@
+<template>
+  <div id="footer">
+    <div class="footer">
+      <div class="footer__contact">
+        <div class="footer__logo">
+          <img src="@/assets/icons/logo.svg" alt="" />
+          <h3 class="footer__heading">grocerymart</h3>
+        </div>
+        <div class="footer__introduce">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, maxime et veniam eligendi rem voluptatibus.
+          </p>
+        </div>
+        <div class="footer__contactEmail">
+          <p>Receive product news and updates</p>
+          <div class="footer__typeEmail">
+            <input type="Email" placeholder="Email address" />
+            <button>SEND</button>
+          </div>
+        </div>
+      </div>
+      <div class="footer__shop">
+        <h1>SHOP</h1>
+        <div class="footer__list">
+          <p>All Department</p>
+          <p>Fashion Deals</p>
+          <p>Electronics Discounts</p>
+          <p>Home & Living Special</p>
+          <p>Beauty Bargains</p>
+        </div>
+      </div>
+      <div class="footer__support">
+        <h1>SUPPORT</h1>
+        <div class="footer__list">
+          <p>Store locator</p>
+          <p>Order status</p>
+        </div>
+      </div>
+      <div class="footer__company">
+        <h1>COMPANY</h1>
+        <div class="footer__list">
+          <p>Customer Service</p>
+          <p>Term of Use</p>
+          <p>Privacy</p>
+          <p>Carrers</p>
+          <p>About</p>
+          <p>Affiliates</p>
+        </div>
+      </div>
+      <div class="footer__hotline">
+        <h1>CONTACT</h1>
+        <div class="footer__list">
+          <div>
+            <p>Email</p>
+            <a href="#">contact@grocerymart.com</a>
+          </div>
+          <div>
+            <p>Hotline</p>
+            <a href="#">18008888</a>
+          </div>
+          <div>
+            <p>Address</p>
+            <a href="#">No. 11D, Lot A10, Nam Trung Yen urban area, Yen Hoa Ward, Cau Giay District, City. Hanoi</a>
+          </div>
+          <div>
+            <p>Hours</p>
+            <p>M - F 08:00am - 06:00pm</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="footer__bottom">
+      <p>© 2010 - 2025 Grocery Mart. All rights reserved.</p>
+      <div class="footer__social">
+        <a href="#" class="footer__social-btn facebook"><img src="@/assets/icons/facebook.svg" alt="" /></a>
+        <a href="#" class="footer__social-btn youtube"><img src="@/assets/icons/youtube.svg" alt="" /></a>
+        <a href="#" class="footer__social-btn tiktok"><img src="@/assets/icons/tiktok.svg" alt="" /></a>
+        <a href="#" class="footer__social-btn twitter"><img src="@/assets/icons/twitter.svg" alt="" /></a>
+        <a href="#" class="footer__social-btn linkedin"><img src="@/assets/icons/linkedin.svg" alt="" /></a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+@import './footer.module.scss';
+</style>
